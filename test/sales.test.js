d562a8d59c5d195c89cba68058958e7c05f08668
Johnatas Henrique <57269681+johnatas-henrique@users.noreply.github.com>
2021-10-16 12:35:28 -0300

    Alterando o teste para banco local

--- test/sales.test.js
+++ test/sales.test.js
@@ -1,7 +1,7 @@
 const frisby = require('frisby');
 const { MongoClient } = require('mongodb');
 
-const mongoDbUrl = 'mongodb://mongodb:27017/StoreManager';
+const mongoDbUrl = 'mongodb://localhost:27017/StoreManager';
 const url = 'http://localhost:3000';
 const invalidId = 99999;
 

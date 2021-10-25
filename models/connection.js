034c3413a369e663dd6e5d741a89ecdd32274d22
LeoLacana <wilson17414@gmail.com>
2021-10-25 20:33:04 -0300

    requisito 5

--- models/connection.js
+++ models/connection.js
@@ -5,8 +5,8 @@ const OPTIONS = {
     useUnifiedTopology: true,
 };
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let db = null;
